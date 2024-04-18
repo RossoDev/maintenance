99bae09169a0d4d4c9a6433474f77e45c529d993
RossoDev <44946921+RossoDev@users.noreply.github.com>
2024-04-18 20:16:34 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,32 @@ function installBot() {
     alert("Grazie per aver scelto bot Italia!");
     window.open(url, '_blank');
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    fetch('http://45.43.163.146:25505/servercount')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('server-count').textContent = data.serverCount;
+        })
+        .catch(error => {
+            console.error('Errore nel recuperare il conteggio dei server:', error);
+            document.getElementById('server-count').textContent = 'Errore nel caricamento';
+        });
+
+    // Ottieni il conteggio dei membri
+    fetch('http://45.43.163.146:25505/membercount')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('member-count').textContent = data.memberCount;
+        })
+        .catch(error => {
+            console.error('Errore nel recuperare il conteggio dei membri:', error);
+            document.getElementById('member-count').textContent = 'Errore nel caricamento';
+        });
+});
+
+function installBot() {
+    alert("Grazie per aver scelto bot Italia!");
+    window.open(url, '_blank');
+}
