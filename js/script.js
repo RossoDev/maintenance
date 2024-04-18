682914a8e9c14f5184a543722ea508be84696a6c
RossoDev <44946921+RossoDev@users.noreply.github.com>
2024-04-18 19:25:06 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const url = "https://discord.com/oauth2/authorize?client_id=875744115772514345&permissions=8&scope=applications.commands%20bot";
 
 function installBot() {
-    alert("Grazie per scegliere bot Italia! Per procedere con l'installazione, visita la nostra pagina GitHub.");
+    alert("Grazie per scegliere bot Italia!");
     window.open(url, '_blank');
 }
