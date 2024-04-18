84222ce11d628fe1cd2539be372d4050e4bd07b3
RossoDev <44946921+RossoDev@users.noreply.github.com>
2024-04-18 21:46:57 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,35 +1,24 @@
-const url = "https://discord.com/oauth2/authorize?client_id=875744115772514345&permissions=8&scope=applications.commands%20bot";
-
-function installBot() {
-    alert("Grazie per aver scelto bot Italia!");
-    window.open(url, '_blank');
-}
-
 document.addEventListener('DOMContentLoaded', function() {
-
     fetch('https://45.43.163.146:25505/servercount')
         .then(response => response.json())
         .then(data => {
-            document.getElementById('server-count').textContent = data.serverCount;
+            document.getElementById('server-count').textContent = data.serverCount + ' server';
         })
         .catch(error => {
-            console.error('Errore nel recuperare il conteggio dei server:', error);
-            document.getElementById('server-count').textContent = 'Errore nel caricamento';
+            document.getElementById('server-count').textContent = 'Non disponibile';
         });
 
-    // Ottieni il conteggio dei membri
     fetch('https://45.43.163.146:25505/membercount')
         .then(response => response.json())
         .then(data => {
-            document.getElementById('member-count').textContent = data.memberCount;
+            document.getElementById('member-count').textContent = data.memberCount + ' utenti';
         })
         .catch(error => {
-            console.error('Errore nel recuperare il conteggio dei membri:', error);
-            document.getElementById('member-count').textContent = 'Errore nel caricamento';
+            document.getElementById('member-count').textContent = 'Non disponibile';
         });
-});
 
-function installBot() {
-    alert("Grazie per aver scelto bot Italia!");
-    window.open(url, '_blank');
-}
+    document.getElementById('install-button').addEventListener('click', () => {
+        alert("Grazie per aver scelto Italia!");
+        window.open('https://discord.com/oauth2/authorize?client_id=875744115772514345&permissions=8&scope=applications.commands%20bot', '_blank');
+    });
+});
